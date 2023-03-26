d05ba694c66755848c84fec6467f3e8a9a3220ce
Eduardo Romero <eduardo.romero@baptisthealth.net>
2023-03-26 13:58:33 -0600

    setup the font and assets in pubspec

---
+++ lib/style/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme {
+  static const Color primaryColor = Color(0xFF20B08E);
+  static const Color secondaryColor = Color(0xFF000);
+  static const Color backgroundColor = Color(0xFF1E1E1E);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
